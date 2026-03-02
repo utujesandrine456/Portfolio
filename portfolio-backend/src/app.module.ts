d7714a4b94728ab07697ab784b2153aac05247a0
utujesandrine456 <utujesandrine456@gmail.com>
2026-03-02 23:01:33 +0200

    Fix repo structure remove submodule

---
+++ portfolio-backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ContactModule } from './contact/contact.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      }
+    }),
+
+    ContactModule
+ ]
+})
+export class AppModule {}
---
+++ portfolio-backend/src/contact/dto/contact.dto.ts
@@ -0,0 +1,5 @@
+export class ContactDTO {
+    name: string;
+    email: string;
+    message: string;
+}
